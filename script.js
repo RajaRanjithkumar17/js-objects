1298456409f1579c197d0cbc3c686d47e9532aee
Ranjith113 <rajaranjithkumar17@gmail.com>
2022-08-28 18:57:03 +0530

    my commit

--- script.js
+++ script.js
@@ -13,19 +13,110 @@ var mobile={
     brand:'realme',
     battery:"4000mah",
     ram:"4gb",
-    type:'tough'
- 
-
+    type:'smartphone',
+    orgin:'china'
+   
 }
+console.log(mobile);
+
 //pen
 var pen={
     type:'ink',
     color:'red',
-    brand:'hero'
+    brand:'hero',
+
 }
+console.log(pen);
 
 //fan
 var fan={
     wings:3,
-    
+    color:'brown',
+    type:'ceiling',
+             
 }
+console.log(fan);
+
+//book
+var book={
+    name:'power of subconsious mind',
+    price:'120/-',
+    pages:'125',
+    writer:'james',
+    type:'litreature'
+}
+console.log(book);
+
+//car
+var car={
+    brand:'volvo',
+    type:'hybrid',
+    color:'blue',
+    model:"a20"
+}
+
+console.log(car);
+
+//speaker
+var speaker={
+    power:'battery',
+    bluetooth:'v5.0',
+    batterycap:"100mah",
+    port:"micro-usb"
+
+}
+console.log(speaker);
+
+//watch
+var watch={
+    display:'tough',
+    screen:'3"',
+    type:'wrist-watch',
+    color:'black'
+}
+console.log(watch);
+
+//watwer bottle
+var bottle={
+    capcity:'1 liter',
+    type:'steel',
+    color:'silver',
+    thermo:'yes'
+}
+console.log(bottle);
+
+//light
+var light={
+    type:'LED',
+    watt:'zero',
+    amp:'2a',
+
+}
+console.log(light);
+//laptop
+var laptop={
+    brand:'samssung',
+    screen:'15 inch',
+    model:'rmx15',
+    battery:'45w'
+}
+console.log(laptop);
+
+//paste
+var paste={
+    brand:'pepsodent',
+    price:'10 rs',
+    contain:'salt',
+    mfg:'hul',
+    weight:'10g'
+}
+console.log(paste);
+
+//oil
+var oil={
+    type:'olive',
+    contain:'100ml',
+    use:'for hair',
+    price:'10 rs'
+}
+console.log(oil);
