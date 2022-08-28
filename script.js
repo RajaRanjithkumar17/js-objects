88ee1ff8c8deba93b8e36c49c05c561a38456b37
Ranjith113 <rajaranjithkumar17@gmail.com>
2022-08-28 18:23:42 +0530

    my commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var bags={
+    color:'red',
+    compartment:4,
+    brand:'winner',
+    type:'handbag'
+}
+//for (const r in bags) {  //get keys
+    console.log(bags[color]);//get values
+//}
+
+// mobile
+var mobile={
+    brand:'realme',
+    battery:"4000mah",
+    ram:"4gb",
+    type:'tough'
+ 
+
+}
+//pen
+var pen={
+    type:'ink',
+    color:'red',
+    brand:'hero'
+}
+
+//fan
+var fan={
+    wings:3,
+    
+}
